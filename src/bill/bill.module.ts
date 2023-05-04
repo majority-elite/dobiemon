683aa2f679c41cd5360120897c147bafa2068371
Godbell <12191579@inha.edu>
2023-05-04 16:56:54 +0900

    feat: integrate postgres bill db

--- src/bill/bill.module.ts
+++ src/bill/bill.module.ts
@@ -3,9 +3,28 @@ import { BillController } from './bill.controller';
 import { BillService } from './bill.service';
 import { BotModule } from '@/bot/bot.module';
 import { SettingsConfigModule } from '@/constants/settings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SettingsConfigService } from '@/constants/settings.service';
 
 @Module({
-  imports: [BotModule, SettingsConfigModule],
+  imports: [
+    BotModule,
+    SettingsConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [SettingsConfigModule],
+      useFactory: (settingsConfigService: SettingsConfigService) => ({
+        type: 'postgres',
+        host: settingsConfigService.dbHost,
+        port: settingsConfigService.dbPort,
+        database: settingsConfigService.billDbName,
+        username: settingsConfigService.billDbUsername,
+        password: settingsConfigService.billDbPassword,
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [SettingsConfigService],
+    }),
+  ],
   controllers: [BillController],
   providers: [BillService],
 })
